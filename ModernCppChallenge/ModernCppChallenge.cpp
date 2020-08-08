827124d7355960052909e36c14a6d75082592e3a
coldstew <coldstew@gmail.com>
2020-08-08 22:31:00 +0900

    first solution commit

---
+++ ModernCppChallenge/ModernCppChallenge.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include <iostream>
+
+int main()
+{
+	int ProblemNum;
+	
+    std::cout << "Which Problem?\n";
+	std::cin >> ProblemNum;
+
+	switch (ProblemNum)
+	{
+		default:
+			std::cout << "You chose nothing!" << std::endl;
+			break;
+	}
+
+	return 0;
+}
